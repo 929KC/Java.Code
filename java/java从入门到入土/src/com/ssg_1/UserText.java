d8140f4e0ab46705f3ef80765b99efe7ced99ca2
929KC <3076675259@qq.com>
2022-01-20 22:40:49 +0800

    类和对象

--- java/java从入门到入土/src/com/ssg_1/UserText.java
+++ java/java从入门到入土/src/com/ssg_1/UserText.java
@@ -1,2 +1,61 @@
-package com.ssg_1;public class UserText {
+package com.ssg_1;
+/*
+*类的属性的使用
+*
+* 属性(成员变量)VS局部变量
+* 1.相同点：
+*           1.1定义变量的格式：数据类型 变量名=变量值
+*           1.2先声明，后使用
+*           1.3变量都有其对应的作用域
+* 2.不同点：
+*           2.1在类中声明的位置不同
+*           属性，直接定义在类的一对{}内
+*          局部变量：声明在方法内，方法形参，代码块内，构造器形参，构造器内部的变量
+*           2.2关于权限修饰符的不同
+*       属性：可以声明属性时，指明其权限，使用权限修饰符
+*       常用的权限修饰符：private,public ,protect,默认值
+*       局部变量：不可以使用权限修饰符
+*       2.3默认初始化值的情况
+*       属性：类的属性，根据其类型，都会有默认值
+*           整型(byte,short,int,long ),0
+*           浮点型：float ，double，0.0
+*           字符型;char,0,或者'\u0000'
+*           布尔值（boolean），false
+*           引用数据类型：(类，数组，接口)null
+*   局部变量：没有默认初始化值
+*           意味着，我们在调用局部变量之前，一定要显式赋值
+*           特别的在形参调用时，赋值即可
+*           2.4在内存中加载的位置
+*           属性;加载到堆空间中
+*           局部变量：加载到栈空间
+* */
+public class UserText {
+    public static void main(String[] args) {
+        User u=new User();
+        System.out.println(u.age);
+        System.out.println(u.arr);
+        System.out.println(u.IsMale);
+        System.out.println(u.name);
+        u.talk("韩语");
+
+    }
+
 }
+
+    class  User{
+         //属性，成员变量
+        String name;
+        int age ;
+        boolean IsMale;
+        char arr;
+public static void talk(String language){//形参局部变量
+
+    System.out.println("我们可以使用"+language+"交流");
+
+}
+public static void eat(){
+    String food="馅饼";//局部变量
+    System.out.println("我可以吃"+food);
+
+}
+    }
--- java/java从入门到入土/src/com/ssg_exe/StudentText.java
+++ java/java从入门到入土/src/com/ssg_exe/StudentText.java
@@ -1,2 +1,55 @@
-package com.ssg_exe;public class StudentText {
+package com.ssg_exe;
+
+import java.util.Arrays;
+
+/*
+* 数组元素可以为任意类型，包括自定义的类类型
+* Math.random()是令系统随机选取大于等于 0.0 且小于 1.0 的伪随机 double 值，是Java语言常用代码。
+Math.random()*b 左边闭区间取值0，右边开区间取值b
+Math.random()*b + 1 左边闭区间取值0+1，右边开区间取值b+1
+* 引用类型的变量;只可能存储两类值：null或者地址值
+* */
+public class StudentText {
+    public static void main(String[] args) {
+            Student[] s=new Student[20];//数组对象
+        for (int i=0;i<s.length;i++)
+        {
+            //给数组组元素赋值
+            s[i]=new Student();
+            //给Student对象的属性赋值
+            s[i].number=(i+1);
+            //年级[1,6]
+            s[i].state=(int)(Math.random()*(6-1+1)+1);
+            //成绩[0,100]
+            s[i].score=(int)(Math.random()*(100-0+1));
+
+        }
+        //遍历学生
+        for (int i=0;i< s.length;i++){
+           /* System.out.println(s[i].number+","+s[i].state+","+s[i].score);*/
+            System.out.println(s[i].info());
+        }
+        System.out.println("*************************");
+        for (int i = 0; i < s.length; i++) {
+            if(s[i].state==3){
+                System.out.println(s[i].info());
+            }
+        }
+        System.out.println("*************************");
+        for (int i = 0; i <s.length-1 ; i++) {
+            for(int j=0;j<s.length-1-i;j++){
+                if(s[j].score>s[j+1].score){
+                    Student temp=s[j];
+                    s[j]=s[j+1];
+                    s[j+1]=temp;
+                }
+            }
+        }
+        for (int i=0;i< s.length;i++){
+            /* System.out.println(s[i].number+","+s[i].state+","+s[i].score);*/
+            System.out.println(s[i].info());
+        }
+
+    }
+
 }
