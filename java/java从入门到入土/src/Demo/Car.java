10da47f6d64e046e90f2a23fe769057f5a196305
929KC <3076675259@qq.com>
2022-01-31 22:22:23 +0800

    构造器

---
+++ java/java从入门到入土/src/Demo/Car.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Car {
+        String name;
+        double price;
+
+    public Car() {
+    }
+
+    public Car(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
