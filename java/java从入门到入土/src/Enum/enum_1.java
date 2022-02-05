9e5e598b218e8b86d644847edcbf75b9f4096d09
929KC <3076675259@qq.com>
2022-02-05 21:01:59 +0800

    枚举

---
+++ java/java从入门到入土/src/Enum/enum_1.java
@@ -0,0 +1,14 @@
+package Enum;
+
+/**
+ * Description:
+ *
+ * @Author KC萧寒
+ * Date: 2022-02-05
+ * Time: 20:14
+ */
+public class enum_1 {
+    enum Season{
+        SPRING,SUMMER,AUTUMN,WINTER;
+    }
+}
