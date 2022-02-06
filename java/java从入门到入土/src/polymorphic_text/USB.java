09ca92272d619182fcbef3c844e7c97113722099
929KC <3076675259@qq.com>
2022-02-06 22:08:12 +0800

    多态

---
+++ java/java从入门到入土/src/polymorphic_text/USB.java
@@ -0,0 +1,13 @@
+package polymorphic_text;
+
+/**
+ * Description:
+ *
+ * @Author KC萧寒
+ * Date: 2022-02-06
+ * Time: 20:38
+ */
+public interface USB {
+    void connect();
+    void unconnect();
+}
