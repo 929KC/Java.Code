ada3f3c13cece1371aa73b88a0ecb8ecfd096caa
929KC <3076675259@qq.com>
2022-02-07 23:09:17 +0800

    匿名内部类

---
+++ java/java从入门到入土/src/innerclass_anonymous/Text.java
@@ -0,0 +1,31 @@
+//package innerclass_anonymous;
+//
+///**
+// * Description:
+// *
+// * @Author KC萧寒
+// * Date: 2022-02-07
+// * Time: 22:14
+// */
+//public class Text {
+//    public static void main(String[] args) {
+//        Swimming s=new Student();
+//
+//    }
+//
+//    class Student implements Swimming{
+//        @Override
+//        public void swim(){
+//            System.out.println("学生游泳");
+//        }
+//    }
+//    public static  void go(Swimming s) {
+//        System.out.println("开始游泳");
+//        s.swim();
+//        System.out.println("结束游泳");
+//    }
+//
+//    interface Swimming{
+//        void swim();
+//    }
+//}
