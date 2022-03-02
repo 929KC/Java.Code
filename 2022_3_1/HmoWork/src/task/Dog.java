16d5ae8aae1edd840b56ce4dfac372e9032431dd
929KC <3076675259@qq.com>
2022-03-02 22:49:22 +0800

    2022_3_1

---
+++ 2022_3_1/HmoWork/src/task/Dog.java
@@ -0,0 +1,66 @@
+package task;
+
+public class Dog {
+    private String name;
+    private String sex;
+    private int heath;
+    private int live;
+
+    public Dog() {
+    }
+
+    public Dog(String name, String sex, int heath, int live) {
+        this.name = name;
+        this.sex = sex;
+        this.heath = heath;
+        this.live = live;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getHeath() {
+        return heath;
+    }
+
+    public void setHeath(int heath) {
+        if(heath<0||heath>100){
+            System.out.println("健康值应该 在0~100之间，默认值为60.");
+            this.heath=60;
+        }else{
+            this.heath = heath;
+        }
+    }
+
+    public int getLive() {
+        return live;
+    }
+
+    public void setLive(int live) {
+        if(live<0||live>100){
+            System.out.println("健康值应该 在0~100之间，默认值为60.");
+            this.live=60;
+        }else{
+            this.live = live;
+        }
+    }
+    public void print(){
+        System.out.println("狗狗的自白：");
+        System.out.println("我的名字叫"+this.name+","+"健康值是"+","
+                +this.heath+","+"和主人的亲密度为"+this.live+
+                ","+"我的性别是"+this.sex+".");
+    }
+}
