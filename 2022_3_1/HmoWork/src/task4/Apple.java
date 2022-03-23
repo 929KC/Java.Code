53009655d77600d3278aa7d34f4f4cfc292f1602
929KC <3076675259@qq.com>
2022-03-23 13:48:55 +0800

    水果工厂

---
+++ 2022_3_1/HmoWork/src/task4/Apple.java
@@ -0,0 +1,8 @@
+package task4;
+
+public  class Apple extends Fruit{
+   @Override
+    public  void productFruit(){
+       System.out.println("生产苹果");
+    }
+}
---
+++ 2022_3_1/HmoWork/src/task4/Fruit.java
@@ -0,0 +1,14 @@
+package task4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * truth:talk  is cheap, show me the code.
+ *
+ * @Author ：KC萧寒
+ * Date: 2022-03-23
+ * Time: 12:12
+ */
+public abstract class Fruit {
+    public abstract void productFruit();
+}
