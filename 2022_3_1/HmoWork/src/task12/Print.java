e171ccbdc0dde3ebf97364c5753a8e397fb15e3f
929KC <3076675259@qq.com>
2022-04-13 22:16:48 +0800

    jdbc

---
+++ 2022_3_1/HmoWork/src/task12/Print.java
@@ -0,0 +1,17 @@
+package task12;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * truth:talk  is cheap, show me the code.
+ *
+ * @Author ：KC萧寒
+ * Date: 2022-04-13
+ * Time: 13:33
+ */
+public class Print {
+   public void print(){
+       System.out.println("hello world");
+   }
+
+}
