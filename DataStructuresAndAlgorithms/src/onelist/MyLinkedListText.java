c0867f9b8ecbbd5bdae8b06eb0ee951d6e212c8a
929KC <3076675259@qq.com>
2022-04-23 21:42:01 +0800

    单链表/顺序表

---
+++ DataStructuresAndAlgorithms/src/onelist/MyLinkedListText.java
@@ -0,0 +1,24 @@
+package onelist;
+
+/**
+ * truth:talk is cheap, show me the code
+ *
+ * @author KC萧寒
+ * @description
+ * @createDate: 2022-04-23 13:29
+ */
+
+public class MyLinkedListText {
+    public static void main(String[] args) {
+        MyLinkedList myList = new MyLinkedList();
+        myList.addFirst(12);
+        myList.addFirst(13);
+        myList.addFirst(14);
+        myList.addFirst(15);
+        myList.addFirst(16);
+        myList.display();
+
+
+
+    }
+}
