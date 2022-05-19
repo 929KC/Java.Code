7900c56d070ebbabf4398fdf7f1b7ddbb86f9a67
929KC <3076675259@qq.com>
2022-05-19 22:09:28 +0800

    图书管理系统

--- 2022/May/5-19/src/operation/AddBook.java
+++ 2022/May/5-19/src/operation/AddBook.java
@@ -21,7 +21,6 @@ public class AddBook implements IOperations {
         String name = sc.next();
         System.out.println("请输入你要新增图书的作者：");
         String author = sc.next();
-        System.out.println("请输入你要新增图书的价格：");
         int price = sc.nextInt();
         System.out.println("请输入你要新增图书的类型：");
         String type = sc.next();
