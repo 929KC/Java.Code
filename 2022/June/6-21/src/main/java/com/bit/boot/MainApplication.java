565658bfd47e13f9b13213f407da9ae6e648d347
929KC <3076675259@qq.com>
2022-06-21 19:58:28 +0800

    boot-Hello-1

---
+++ 2022/June/6-21/src/main/java/com/bit/boot/MainApplication.java
@@ -0,0 +1,18 @@
+package com.bit.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * truth:talk is cheap, show me the code
+ *
+ * @author KC萧寒
+ * @description
+ * @createDate: 2022-06-21 09:30
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
