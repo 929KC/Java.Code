dd1a03292005964dbff9afcb2075c3531c150589
929KC <3076675259@qq.com>
2022-07-13 22:16:10 +0800

    力扣-110-二进制转十进制

---
+++ 2022/7-13/src/main/java/com/bit/lc/Demo.java
@@ -0,0 +1,17 @@
+package com.bit.lc;
+
+/**
+ * truth:talk is cheap, show me the code
+ *
+ * @author KC萧寒
+ * @description
+ * @createDate: 2022-07-13 20:03
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        int num=Integer.valueOf("101",2);
+        System.out.println(num);
+
+    }
+}
