2915e2a7cefcf9ef791b4952c0e93058c989a4a2
929KC <3076675259@qq.com>
2022-07-22 13:50:54 +0800

    测试

---
+++ 2022/July/src/main/java/com/test.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * truth:talk is cheap, show me the code
+ *
+ * @author KC萧寒
+ * @description
+ * @createDate: 2022-07-22 13:50
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
